ae242af9551929bd51f93f8ee9a216f2b9ca1148
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-07-09 02:35:44 -0300

    install + leitura de apikey
    instalei todas as dependências do projeto, fiz com o programa consuma a minha api do supebase

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://ltovkgdrqlvdzgvtjxsa.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0b3ZrZ2RycWx2ZHpndnRqeHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MTAzMzYsImV4cCI6MjA2NzQ4NjMzNn0.24iqlv0rC-V6nwL8nEP_0DCnwUV2jNoGeOVbG94EF2c",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0b3ZrZ2RycWx2ZHpndnRqeHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MTAzMzYsImV4cCI6MjA2NzQ4NjMzNn0.24iqlv0rC-V6nwL8nEP_0DCnwUV2jNoGeOVbG94EF2c"
     }
 })
+
+
